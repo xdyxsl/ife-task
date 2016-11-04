56bfe7960d61c9da5406ad40e3190162c9dd2c72
PengJunyi <a214161398@hotmail.com>
2016-11-04 11:57:10 +0800

    update

--- 2016_spring/part2/task16/task.js
+++ 2016_spring/part2/task16/task.js
@@ -15,15 +15,14 @@ var aqiData = {};
 function addAqiData() {
     var city = document.getElementById('aqi-city-input').value;
     if (!city.match(/^[a-zA-Z\u4e00-\u9fa5]+$/)) {
-        alert("城市名称只接受中英文字符哦。");
+        alert("城市名称只接受中英文字符!");
         return false;
     }
     var value = document.getElementById('aqi-value-input').value;
     if (!value.match(/^-?\d+$/)) {
-        alert("空气质量只接受整数哦");
+        alert("空气质量只接受整数!");
         return false;
     }
-    console.log(city, value)
     aqiData[city] = value;
 }
 
@@ -43,8 +42,10 @@ function renderAqiList() {
  * 获取用户输入，更新数据，并进行页面呈现的更新
  */
 function addBtnHandle() {
-    addAqiData();
-    renderAqiList();
+    var confirm = addAqiData();
+    if (confirm != false) {
+        renderAqiList();
+    }
 }
 
 /**
@@ -52,8 +53,6 @@ function addBtnHandle() {
  * 获取哪个城市数据被删，删除数据，更新表格显示
  */
 function delBtnHandle() {
-    // do sth.
-
     //通过当前元素父节点的父节点的第一个子节点的文本内容确定当前数组的属性并删除
     //这里不能用e，要用this来指代对象
     var cityName = this.target.parentNode.parentNode.childNodes[0].innerHTML;
@@ -62,23 +61,19 @@ function delBtnHandle() {
 }
 var event = event || window.event;
 
-function init() {
+(function init() {
     // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
     document.getElementById('add-btn').addEventListener("click", addBtnHandle, false)
 
     // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
-
     //为表格绑定事件监听，if筛选出button按钮，并为button按钮添加删除函数
     document.getElementById("aqi-table").addEventListener("click", function(e) {
         //下两行适配FF和IE，将event作为函数的参数传入例如e，然后为参数e添加适配方法
         e = e || window.event;
         target = e.target || e.srcElement;
-
         if (e.target.nodeName.toLowerCase() == "button") {
             delBtnHandle();
         }
     }, false)
+})()
 
-}
-
-init();
